5aa877e9f1cfb02890474688652aeeacd46f1c72
dongliang <liangdong@lilith.com>
2022-05-30 15:53:42 +0800

    feat: init repo

---
+++ packages/web-design/src/antd-config.ts
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    "primary-color": "#5D84E0",
+    "text-color": "#303133",
+    "text-color-secondary": "#606266",
+    "disabled-color": "#BFBFBF",
+    "border-radius-base": "8px",
+  },
+  navTheme: "light",
+};
